9e9225d2a98a735c5350af784c3aa75ee27af752
Yangzom Dolma <Yangzom.dolma-1@ou.edu>
2019-02-06 14:52:26 -0600

    Initial Commit

---
+++ TestPr1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TestPr1
+//
+//  Created by Apotsang Yangzom on 2/6/19.
+//  Copyright © 2019 Apotsang Yangzom. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
